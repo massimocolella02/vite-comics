a5acff9c6be5525cf60a3ab32e4c516c0e6eaa11
Massimo Colella <massimo-colella@outlook.it>
2023-05-04 19:06:06 +0200

    esercizio completato

---
+++ src/components/CardComp.vue
@@ -0,0 +1,29 @@
+<script>
+export default{
+    name: "CardComp",
+    props: ['dettaglicards']
+}
+</script>
+
+<template>
+    <div class="card">
+        <img :src="dettaglicards.thumb" class="card-img-top" alt="">
+        <div>
+            <h6 class="card-title">{{dettaglicards.series}}</h6>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.card{
+    width: calc(100% / 6 - 10px);
+    background-color: transparent;
+    border: none;
+    color: white;
+    text-transform: uppercase;
+    img{
+        height: 300px;
+        margin-bottom: 5px;
+    }
+}
+</style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,9 +1,87 @@
 <script>
+import CardComp from "./CardComp.vue"
+
 export default{
     name: "MainComp",
+    components:{
+        CardComp,
+    },
     data() {
         return {
-            
+            Mycards:[
+                    {
+                        "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                        "price": "$19.99",
+                        "series": "Action Comics",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                        "price": "$3.99",
+                        "series": "American Vampire 1976",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                        "price": "$16.99",
+                        "series": "Aquaman",
+                        "type": "graphic novel",
+                    },
+                    {
+                        "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                        "price": "$2.99",
+                        "series": "Batgirl",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                        "price": "$3.99",
+                        "series": "Batman",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                        "price": "$2.99",
+                        "series": "Batman Beyond",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                        "price": "$3.99",
+                        "series": "Batman/Superman",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                        "price": "$4.99",
+                        "series": "Batman/Superman Annual",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                        "price": "$5.99",
+                        "series": "Batman: The Joker War Zone",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                        "price": "$6.99",
+                        "series": "Batman: Three Jokers",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                        "price": "$4.99",
+                        "series": "Batman: White Knight Presents: Harley Quinn",
+                        "type": "comic book",
+                    },
+                    {
+                        "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                        "price": "$16.99",
+                        "series": "Catwoman",
+                        "type": "graphic novel",
+                    },
+                ]
         }
     },
 }
@@ -11,21 +89,54 @@ export default{
 
 <template>
     <main>
-        <div class="container">
-            <h2>Qua vanno le immagini</h2>
+        <div class="jumbotron">
+            <span>current series</span>
         </div>
+
+        <div class="cards">
+            <div class="container">
+                <CardComp v-for="(element, index) in Mycards" :key="index" :dettaglicards="element"/>
+            </div>
+
+            <button class="btn btn-primary mb-5">LOAD MORE</button>
+        </div>
+
     </main>
 </template>
 
 <style scoped lang="scss">
+@use "../style/variables" as *;
 main{
-    background-color: #1C1C1C;
-    padding: 50px 0;
-    .container{
-        h2{
-            color: whitesmoke;
+    .jumbotron{
+        height: 400px;
+        background-image: url('/img/jumbotron.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+        position: relative;
+        span{
+            padding: 7px;
+            position: absolute;
+            background-color: $blue;
+            border: 1px solid transparent;
+            text-transform: uppercase;
+            color: white;
+            bottom: -20px;
+            left: 20%;
+        }
+    }
+
+    .cards{
+        background-color: #1C1C1C;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        .container{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-wrap: wrap;
+            padding: 50px 0;
         }
     }
 }
-
 </style>
